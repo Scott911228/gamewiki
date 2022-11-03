792fd4e62d223567c1682dd15016610718264b25
Scott911228 <95304156+Scott911228@users.noreply.github.com>
2022-11-03 21:01:43 +0800

    Update arknights.js

--- js/arknights.js
+++ js/arknights.js
@@ -5,13 +5,14 @@ $(document).ready(function(){
     $('.gallery2_item-link ').mouseenter(function(){
 
         var title = $(this).parent('li').data('title');
+        var content = $(this).parent('li').data('content');
         if(!$(this).children('div').length){
             $(this).append('<div class="overlay"></div>');
         }
 
         var overlay = $(this).children('.overlay');
 
-        overlay.html( title );
+        overlay.html('<h2>' + title + '</h2><p>' + content + '</p>' );
 
         overlay.fadeIn(500);
     });
